14af8a3f91d53f3560fabb4ef38e9ab4bef9f18f
Sljivoa14 <amarsljivo7@gmail.com>
2026-07-24 18:54:47 +0200

    settings, updtated

--- cop.py
+++ cop.py
@@ -4,7 +4,7 @@ cop.py file
 COP NPC behavior. cop appears when we did crimes(hurt npcs, and more will be added).
 """
 import pygame
-import npc from npc #will be fixed tommowrow
+import npc from NPC #will be fixed tommowrow
 
 from settings import (
     TILE_SIZE,
