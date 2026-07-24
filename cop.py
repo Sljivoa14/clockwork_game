5319bddcf6c02cf8a49675a50bc2986b758b3f66
Sljivoa14 <amarsljivo7@gmail.com>
2026-07-24 18:53:28 +0200

    added a cop.py and now working on making tG better

---
+++ cop.py
@@ -0,0 +1,16 @@
+"""
+cop.py file
+-
+COP NPC behavior. cop appears when we did crimes(hurt npcs, and more will be added).
+"""
+import pygame
+import npc from npc #will be fixed tommowrow
+
+from settings import (
+    TILE_SIZE,
+    COP_SPEED,
+    COP_CHASE_RANGE,
+    COP_CONTACT_DAMAGE_CD,
+
+)
+
